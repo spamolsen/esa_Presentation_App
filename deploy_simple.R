4c4ff113ddc3f231c1aad0cb14be70137ae40ffc
Sammy Olsen <ssolsen10@gmail.com>
2025-08-31 16:06:31 -0400

    Remove sensitive account info from deploy_simple.R

--- deploy_simple.R
+++ deploy_simple.R
@@ -2,9 +2,7 @@ library(rsconnect)
 
 # Set account info
 rsconnect::setAccountInfo(
-  name='sammyolsen',
-  token='3CB2E33B473DA1755BAB69CA89FEE262',
-  secret='4VOaREiPAv4oR9GqFfGnXF+nibdwpVyM/kqTxkGI'
+
 )
 
 # Deploy the app
