c3e2f390c5f9ed6e9f142ef62185e3899e1bfce5
Sammy Olsen <ssolsen10@gmail.com>
2025-08-31 16:05:31 -0400

    Remove sensitive account info from deploy script

--- deploy_with_renv.R
+++ deploy_with_renv.R
@@ -2,9 +2,6 @@ library(rsconnect)
 
 # Set account info
 rsconnect::setAccountInfo(
-  name='sammyolsen',
-  token='3CB2E33B473DA1755BAB69CA89FEE262',
-  secret='4VOaREiPAv4oR9GqFfGnXF+nibdwpVyM/kqTxkGI'
 )
 
 # Initialize renv if not already initialized
